b87e0984a0861c15f7bb7d1a5f0b14158647f8ec
BarryYan <barry0310yan@gmail.com>
2022-11-22 09:24:07 +0800

    eval

---
+++ eval.py
@@ -0,0 +1,13 @@
+import subprocess
+
+if __name__ == "__main__":
+    topo = ["NEW_EliBackbone/EVALUATE", "NEW_Janetbackbone/EVALUATE", "NEW_HurricaneElectric/EVALUATE"]
+    log = "Enero_3top_15_B_NEW"
+    for t in topo:
+        subprocess.call(["python", "eval_on_single_topology.py",
+                         "-max_edge", "100", "-min_edge", "5",
+                         "-max_nodes", "30", "-min_nodes", "1",
+                         "-n", "2",
+                         "-f1", "results_single_top", "-f2", topo,
+                         "-d", f"./Logs/exp{log}Logs.txt"], shell=True)
+    subprocess.call(["python", "figures_5_and_6.py", "-d", log], shell=True)
